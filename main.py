0e8c4c3c6cbb6167071090c13917a1ef3196863d
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-02-19 17:58:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 # python3
 
 from collections import namedtuple
+import os
 
 Bracket = namedtuple("Bracket", ["char", "position"])
 
-
+def read_text_file(file_path):
+    with open(file_path, 'r') as f:
+        return f.read()
+  
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
 def find_mismatch(text):
-    result = 0
+    #result = 0
+    isBracketFound = False
     opening_brackets_stack = []
-    
     
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
+            isBracketFound = True
             left = Bracket(next, i)
             opening_brackets_stack.append(left)
             
             pass
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
-        
+            isBracketFound = True
             if len(opening_brackets_stack) != 0:
                 lastElement = opening_brackets_stack[len(opening_brackets_stack) - 1]
                 if are_matching(lastElement.char, next) : 
                     opening_brackets_stack.remove(lastElement)
                 else :
-                    result = i + 1
-                    return result
+                    #result = i + 1
+                    return i + 1
                 return 'Success'
-    if result == 0: 
-        return 1  
+    if isBracketFound == False:
+        return 'Success'
+    else:
+        return 1
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
-    print(mismatch)
+    path = os.getcwd() + '/test'
+    os.chdir(path)
+    check = input()
+    if check == 'F':
+        for file in os.listdir():
+            file_path = f"{path}\{file}"
+            text = read_text_file(file_path)
+            mismatch = find_mismatch(text)
+            print(mismatch)
+    if check == "I":
+        text = input()
+        mismatch = find_mismatch(text)
+        print(mismatch)
 
 if __name__ == "__main__":
-    
     main()
